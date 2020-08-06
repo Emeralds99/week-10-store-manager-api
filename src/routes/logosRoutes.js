295531796fcdaa841d16d7350d7e01d3bb3d5883
Peter Harrington <pharrington375@gmail.com>
2020-08-06 19:27:28 -0400

    Route put, post and delete to logos DB file

--- src/routes/logosRoutes.js
+++ src/routes/logosRoutes.js
@@ -6,4 +6,26 @@ router.get('/', async (req, res) => {
   res.send(await getLogos());
 });
 
+router.post('/', async (apiRequest, apiResponse) => {
+  const newLogo = apiRequest.body;
+  await createLogo(newLogo);
+  apiResponse.send({
+    message: 'New logo created.',
+    allLogos: await getLogos(),
+  });
+});
+
+router.delete('/:logoId', async (apiRequest, apiResponse) => {
+  await deletelogo(apiRequest.params.logoId);
+  apiResponse.send({ message: 'logo deleted.' });
+});
+
+// endpoint to update a logo
+router.put('/:id', async (apiRequest, apiResponse) => {
+  const updatedlogo = apiRequest.body;
+  console.log({ updatedlogo})
+  await updatelogo(apiRequest.params.id, updatedlogo);
+  apiResponse.send({ message: 'logo updated.' });
+});
+
 module.exports = router;
